24fd6fbec4e173f91a8fd836ac0ef6fda72b08e6
Jakub Kowalczyk <beufort@o2.pl>
2016-10-26 11:49:21 +0200

    added exerciseservuce and exerciseserviceimpl

---
+++ src/main/java/pl/workout/kowalczyk/com/app/services/service/ExerciseService.java
@@ -0,0 +1,16 @@
+package pl.workout.kowalczyk.com.app.services.service;
+
+import pl.workout.kowalczyk.com.app.enums.ExerciseType;
+import pl.workout.kowalczyk.com.app.model.data.model.entity.Exercise;
+
+import java.util.List;
+
+/**
+ * Created by JK on 2016-10-26.
+ */
+public interface ExerciseService {
+    void saveExercise(Exercise exercise);
+    void getAllExercises();
+    Exercise getExerciseByName(String name);
+    List<Exercise> getExercisesForBodyPart(ExerciseType exerciseType);
+}
---
+++ src/main/java/pl/workout/kowalczyk/com/app/services/serviceImpl/ExerciseServiceImpl.java
@@ -0,0 +1,37 @@
+package pl.workout.kowalczyk.com.app.services.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.workout.kowalczyk.com.app.enums.ExerciseType;
+import pl.workout.kowalczyk.com.app.model.data.model.dao.ExerciseDao;
+import pl.workout.kowalczyk.com.app.model.data.model.entity.Exercise;
+import pl.workout.kowalczyk.com.app.services.service.ExerciseService;
+
+import java.util.List;
+
+/**
+ * Created by JK on 2016-10-26.
+ */
+@Service
+@Transactional
+public class ExerciseServiceImpl implements ExerciseService {
+    @Autowired
+    private ExerciseDao exerciseDao;
+
+    public void saveExercise(Exercise exercise) {
+        exerciseDao.save(exercise);
+    }
+
+    public void getAllExercises() {
+        exerciseDao.getAll();
+    }
+
+    public Exercise getExerciseByName(String name) {
+        return exerciseDao.getExerciseByName(name);
+    }
+
+    public List<Exercise> getExercisesForBodyPart(ExerciseType exerciseType) {
+        return exerciseDao.getExercisesForBodyPart(exerciseType);
+    }
+}
