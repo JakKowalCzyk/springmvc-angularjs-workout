dec210eaf91ec97fee22a1f553861e7699ab1e69
Jakub Kowalczyk <beufort@o2.pl>
2016-10-26 12:37:03 +0200

    added UserNotesService and impl

---
+++ src/main/java/pl/workout/kowalczyk/com/app/services/service/UserNotesService.java
@@ -0,0 +1,21 @@
+package pl.workout.kowalczyk.com.app.services.service;
+
+import pl.workout.kowalczyk.com.app.model.data.model.entity.UserNotes;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Created by JK on 2016-10-26.
+ */
+public interface UserNotesService {
+    void saveUserNotes(UserNotes userNotes);
+
+    void updateUserNotes(UserNotes userNotes);
+
+    void deleteUserNotes(UserNotes userNotes);
+
+    List<UserNotes> getUserNotesByUserId(int userId);
+
+    UserNotes getSingleNoteByDate(int userId, Date date);
+}
---
+++ src/main/java/pl/workout/kowalczyk/com/app/services/serviceImpl/UserNotesServiceImpl.java
@@ -0,0 +1,41 @@
+package pl.workout.kowalczyk.com.app.services.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.workout.kowalczyk.com.app.model.data.model.dao.UserNotesDao;
+import pl.workout.kowalczyk.com.app.model.data.model.entity.UserNotes;
+import pl.workout.kowalczyk.com.app.services.service.UserNotesService;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Created by JK on 2016-10-26.
+ */
+@Service
+@Transactional
+public class UserNotesServiceImpl  implements UserNotesService{
+    @Autowired
+    private UserNotesDao userNotesDao;
+
+    public void saveUserNotes(UserNotes userNotes) {
+        userNotesDao.save(userNotes);
+    }
+
+    public void updateUserNotes(UserNotes userNotes) {
+        userNotesDao.update(userNotes);
+    }
+
+    public void deleteUserNotes(UserNotes userNotes) {
+        userNotesDao.delete(userNotes);
+    }
+
+    public List<UserNotes> getUserNotesByUserId(int userId) {
+        return userNotesDao.getUserNotesByUserId(userId);
+    }
+
+    public UserNotes getSingleNoteByDate(int userId, Date date) {
+        return userNotesDao.getSingleNoteByDate(userId, date);
+    }
+}
