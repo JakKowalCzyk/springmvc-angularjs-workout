81dacfdffc0e03af2123441dbeef5eb38ccc89a7
Jakub Kowalczyk <beufort@o2.pl>
2016-10-26 11:46:41 +0200

    added @repository

--- src/main/java/pl/workout/kowalczyk/com/app/model/data/model/dao/UserInfoDao.java
+++ src/main/java/pl/workout/kowalczyk/com/app/model/data/model/dao/UserInfoDao.java
@@ -1,5 +1,6 @@
 package pl.workout.kowalczyk.com.app.model.data.model.dao;
 
+import org.springframework.stereotype.Repository;
 import pl.workout.kowalczyk.com.app.model.data.model.entity.FavouriteExercise;
 import pl.workout.kowalczyk.com.app.model.data.model.entity.UserInfo;
 import pl.workout.kowalczyk.com.app.model.data.model.entity.UserWeight;
@@ -7,6 +8,7 @@ import pl.workout.kowalczyk.com.app.model.data.model.entity.UserWeight;
 /**
  * Created by JK on 2016-09-22.
  */
+@Repository
 public interface UserInfoDao extends BaseDao<UserInfo> {
     UserInfo getUserInfoByUserId(int userId);
 
