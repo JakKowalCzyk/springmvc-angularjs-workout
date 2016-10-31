bebd4f70c0c17083f3b1e4ee59fa017ad7e56b18
Jakub Kowalczyk <beufort@o2.pl>
2016-10-31 16:16:39 +0100

    reverted delete of favouriteExerciseDao

---
+++ src/main/java/pl/workout/kowalczyk/com/app/model/data/dao/FavouriteExerciseDao.java
@@ -0,0 +1,11 @@
+package pl.workout.kowalczyk.com.app.model.data.dao;
+
+import pl.workout.kowalczyk.com.app.model.data.dao.BaseDao;
+import pl.workout.kowalczyk.com.app.model.data.entity.FavouriteExercise;
+
+/**
+ * Created by JK on 2016-09-22.
+ */
+public interface FavouriteExerciseDao extends BaseDao<FavouriteExercise> {
+
+}
