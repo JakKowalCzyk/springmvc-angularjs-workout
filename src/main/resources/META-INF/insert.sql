4f39995aba53cbdeb825f6fa4636171c1d10ae23
Jakub Kowalczyk <beufort@o2.pl>
2016-11-05 19:43:52 +0100

    changed creation of table to creation by hibernate

--- src/main/resources/META-INF/insert.sql
+++ src/main/resources/META-INF/insert.sql
@@ -1,23 +1,23 @@
-drop table if EXISTS saw_user CASCADE ;
-drop table if EXISTS saw_userInfo CASCADE ;
-drop table if EXISTS saw_userWeight CASCADE ;
-drop table if EXISTS saw_userNotes CASCADE ;
-drop table if EXISTS saw_exercise CASCADE ;
-drop table if EXISTS saw_userExercise CASCADE ;
-
-create table saw_user(user_id SERIAL NOT NULL PRIMARY KEY, login VARCHAR(255), firstName VARCHAR(255), lastName VARCHAR(255), birthDay DATE , email VARCHAR(255), password VARCHAR(255));
-
-CREATE TABLE saw_userWeight(uweight_id SERIAL not null PRIMARY KEY,user_id int REFERENCES saw_user(user_id), weight_kg int, date DATE);
-
-CREATE TABLE saw_exercise(exercise_id SERIAL not null PRIMARY KEY, name VARCHAR(255), description text, exerciseType VARCHAR(255));
-
-CREATE TABLE saw_workout(workout_id SERIAL NOT NULL PRIMARY KEY ,user_id int REFERENCES saw_user(user_id), date DATE);
-
-CREATE TABLE saw_userExercise(usexercise_id SERIAL NOT NULL PRIMARY key, exercise_id int REFERENCES saw_exercise(exercise_id), workout_id int REFERENCES saw_workout(workout_id), repeat int, series int);
-
-create table saw_userInfo(uinfo_id SERIAL NOT NULL PRIMARY KEY, user_id int REFERENCES saw_user(user_id), actual_weight int REFERENCES saw_userWeight(uweight_id), efavourite_id int REFERENCES saw_exercise(exercise_id));
-
-CREATE TABLE saw_userNotes(unotes_id SERIAL NOT NULL PRIMARY key, user_id int REFERENCES saw_user(user_id), note text, date DATE );
+-- drop table if EXISTS saw_user CASCADE ;
+-- drop table if EXISTS saw_userInfo CASCADE ;
+-- drop table if EXISTS saw_userWeight CASCADE ;
+-- drop table if EXISTS saw_userNotes CASCADE ;
+-- drop table if EXISTS saw_exercise CASCADE ;
+-- drop table if EXISTS saw_userExercise CASCADE ;
+--
+-- create table saw_user(user_id SERIAL NOT NULL PRIMARY KEY, login VARCHAR(255), firstName VARCHAR(255), lastName VARCHAR(255), birthDay DATE , email VARCHAR(255), password VARCHAR(255));
+--
+-- CREATE TABLE saw_userWeight(uweight_id SERIAL not null PRIMARY KEY,user_id int REFERENCES saw_user(user_id), weight_kg int, date DATE);
+--
+-- CREATE TABLE saw_exercise(exercise_id SERIAL not null PRIMARY KEY, name VARCHAR(255), description text, exerciseType VARCHAR(255));
+--
+-- CREATE TABLE saw_workout(workout_id SERIAL NOT NULL PRIMARY KEY ,user_id int REFERENCES saw_user(user_id), date DATE);
+--
+-- CREATE TABLE saw_userExercise(usexercise_id SERIAL NOT NULL PRIMARY key, exercise_id int REFERENCES saw_exercise(exercise_id), workout_id int REFERENCES saw_workout(workout_id), repeat int, series int);
+--
+-- create table saw_userInfo(uinfo_id SERIAL NOT NULL PRIMARY KEY, user_id int REFERENCES saw_user(user_id), actual_weight int REFERENCES saw_userWeight(uweight_id), efavourite_id int REFERENCES saw_exercise(exercise_id));
+--
+-- CREATE TABLE saw_userNotes(unotes_id SERIAL NOT NULL PRIMARY key, user_id int REFERENCES saw_user(user_id), note text, date DATE );
 
 INSERT INTO saw_user (login, firstName, lastName) VALUES ('kuba', 'kowalczyk', 'kowal');
 INSERT INTO saw_workout(user_id, date) VALUES (1, '2015-02-02');
@@ -27,6 +27,7 @@ INSERT INTO saw_exercise (name, exerciseType) VALUES ('Wykroki', 'LEGS');
 INSERT INTO saw_userExercise (exercise_id, workout_id, repeat, series) VALUES (1, 1, 10, 3);
 INSERT INTO saw_userExercise (exercise_id, workout_id, repeat, series) VALUES (2, 1, 10, 3);
 INSERT INTO saw_userExercise (exercise_id, workout_id, repeat, series) VALUES (3, 1, 10, 3);
+INSERT INTO saw_userinfo (user_id) VALUES (1);
 
 
 
