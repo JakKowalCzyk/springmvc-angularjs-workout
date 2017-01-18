464ef24e859e7e68015e073511f800745184a941
Jakub Kowalczyk <beufort@o2.pl>
2017-01-18 18:24:17 +0100

    Spring boot migration successful finished

---
+++ src/main/java/pl/workout/kowalczyk/com/app/AppRunner.java
@@ -0,0 +1,16 @@
+package pl.workout.kowalczyk.com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by JK on 2017-01-02.
+ */
+@EnableWebSecurity
+@SpringBootApplication
+public class AppRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(AppRunner.class, args);
+    }
+}
