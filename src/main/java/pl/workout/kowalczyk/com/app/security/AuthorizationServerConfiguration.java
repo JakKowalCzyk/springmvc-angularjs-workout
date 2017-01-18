6e9ed9d7c560bde1a133317d08cea296f14de042
Jakub Kowalczyk <beufort@o2.pl>
2017-01-18 18:23:34 +0100

    Spring security + oauth2 works with spring boot

--- src/main/java/pl/workout/kowalczyk/com/app/security/AuthorizationServerConfiguration.java
+++ src/main/java/pl/workout/kowalczyk/com/app/security/AuthorizationServerConfiguration.java
@@ -1,7 +1,6 @@
 package pl.workout.kowalczyk.com.app.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
