7b209ec097f487d0db2e8d037a761f9285909844
Jakub Kowalczyk <beufort@o2.pl>
2017-01-18 18:22:57 +0100

    Fixed table names, column names, deleted unused code

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,48 +1,27 @@
--- drop table if EXISTS saw_user CASCADE ;
--- drop table if EXISTS saw_userInfo CASCADE ;
--- drop table if EXISTS saw_userWeight CASCADE ;
--- drop table if EXISTS saw_userNotes CASCADE ;
--- drop table if EXISTS saw_exercise CASCADE ;
--- drop table if EXISTS saw_userExercise CASCADE ;
+-- drop table if EXISTS user CASCADE ;
+-- drop table if EXISTS userInfo CASCADE ;
+-- drop table if EXISTS userWeight CASCADE ;
+-- drop table if EXISTS userNotes CASCADE ;
+-- drop table if EXISTS userExerciseId CASCADE ;
+-- drop table if EXISTS user_exercise CASCADE ;
 --
--- create table saw_user(user_id SERIAL NOT NULL PRIMARY KEY, login VARCHAR(255), firstName VARCHAR(255), lastName VARCHAR(255), birthDay DATE , email VARCHAR(255), password VARCHAR(255));
+-- create table user(userId SERIAL NOT NULL PRIMARY KEY, login VARCHAR(255), firstName VARCHAR(255), lastName VARCHAR(255), birthDay DATE , email VARCHAR(255), password VARCHAR(255));
 --
--- CREATE TABLE saw_userWeight(uweight_id SERIAL not null PRIMARY KEY,user_id int REFERENCES saw_user(user_id), weight_kg int, date DATE);
+-- CREATE TABLE userWeight(uweight_id SERIAL not null PRIMARY KEY,userId int REFERENCES user(userId), weightKg int, date DATE);
 --
--- CREATE TABLE saw_exercise(exercise_id SERIAL not null PRIMARY KEY, name VARCHAR(255), description text, exerciseType VARCHAR(255));
+-- CREATE TABLE userExerciseId(exercise_id SERIAL not null PRIMARY KEY, name VARCHAR(255), description text, exerciseType VARCHAR(255));
 --
--- CREATE TABLE saw_workout(workout_id SERIAL NOT NULL PRIMARY KEY ,user_id int REFERENCES saw_user(user_id), date DATE);
+-- CREATE TABLE workout(workoutId SERIAL NOT NULL PRIMARY KEY ,userId int REFERENCES user(userId), date DATE);
 --
--- CREATE TABLE saw_userExercise(usexercise_id SERIAL NOT NULL PRIMARY key, exercise_id int REFERENCES saw_exercise(exercise_id), workout_id int REFERENCES saw_workout(workout_id), repeat int, series int);
+-- CREATE TABLE userExercise(usexercise_id SERIAL NOT NULL PRIMARY key, exercise_id int REFERENCES userExerciseId(exercise_id), workoutId int REFERENCES workout(workoutId), repeat int, series int);
 --
--- create table saw_userInfo(uinfo_id SERIAL NOT NULL PRIMARY KEY, user_id int REFERENCES saw_user(user_id), actual_weight int REFERENCES saw_userWeight(uweight_id), efavourite_id int REFERENCES saw_exercise(exercise_id));
+-- create table userInfo(uinfo_id SERIAL NOT NULL PRIMARY KEY, userId int REFERENCES user(userId), actualWeight int REFERENCES userWeight(uweight_id), efavourite_id int REFERENCES userExerciseId(exercise_id));
 --
--- CREATE TABLE saw_userNotes(unotes_id SERIAL NOT NULL PRIMARY key, user_id int REFERENCES saw_user(user_id), note text, date DATE );
--- drop TABLE if EXISTS oauth_client_details;
--- drop TABLE if EXISTS oauth_client_token;
+-- CREATE TABLE userNotes(unotes_id SERIAL NOT NULL PRIMARY key, userId int REFERENCES user(userId), note text, date DATE );
+
 -- drop TABLE if EXISTS oauth_access_token;
 -- drop TABLE if EXISTS oauth_refresh_token;
--- drop TABLE if EXISTS oauth_code;
--- CREATE TABLE oauth_client_details (
---   client_id VARCHAR(256) PRIMARY KEY,
---   resource_ids VARCHAR(256),
---   client_secret VARCHAR(256),
---   scope VARCHAR(256),
---   authorized_grant_types VARCHAR(256),
---   web_server_redirect_uri VARCHAR(256),
---   authorities VARCHAR(256),
---   access_token_validity INTEGER,
---   refresh_token_validity INTEGER,
---   additional_information VARCHAR(4096),
---   autoapprove VARCHAR(256)
--- );
--- CREATE TABLE oauth_client_token (
---   token_id VARCHAR(256),
---   token bytea,
---   authentication_id VARCHAR(256),
---   user_name VARCHAR(256),
---   client_id VARCHAR(256)
--- );
+
 -- CREATE TABLE oauth_access_token (
 --   token_id VARCHAR(256),
 --   token bytea,
@@ -57,28 +36,26 @@
 --   token bytea,
 --   authentication bytea
 -- );
--- CREATE TABLE oauth_code (
---   code VARCHAR(256), authentication bytea
--- );
-INSERT INTO saw_user_details (enabled, login, password) VALUES (TRUE, 'nowy', '123');
-INSERT INTO saw_user_details (enabled, login, password) VALUES (TRUE, 'nowy2', '1234');
-INSERT INTO saw_role (name) VALUES ('USER');
-INSERT INTO saw_user_role (user_id, role_id) VALUES (1, 1);
+
+INSERT INTO user_details (enabled, login, password) VALUES (TRUE, 'nowy', '123');
+INSERT INTO user_details (enabled, login, password) VALUES (TRUE, 'nowy2', '1234');
+INSERT INTO role (name) VALUES ('USER');
+INSERT INTO user_role (user_details_id, role_id) VALUES (1, 1);
 INSERT INTO saw_user (user_details_id, first_name, last_name) VALUES (1, 'kowalczyk', 'kowal');
 INSERT INTO saw_user (user_details_id, first_name, email) VALUES (2, 'nowy2', '123');
-INSERT INTO saw_workout(user_details_id, date) VALUES (1, '2015-02-02');
-INSERT INTO saw_workout(user_details_id, date) VALUES (1, '2015-03-03');
-INSERT INTO saw_exercise (name, exercise_type) VALUES ('Przysiady', 'LEGS');
-INSERT INTO saw_exercise (name, exercise_type) VALUES ('Przysiady przednie', 'LEGS');
-INSERT INTO saw_exercise (name, exercise_type) VALUES ('Wykroki', 'LEGS');
-INSERT INTO saw_userExercise (exercise_id, workout_id, repeat, series) VALUES (1, 1, 10, 3);
-INSERT INTO saw_userExercise (exercise_id, workout_id, repeat, series) VALUES (2, 1, 10, 3);
-INSERT INTO saw_userExercise (exercise_id, workout_id, repeat, series) VALUES (3, 1, 10, 3);
-INSERT INTO saw_userExercise (exercise_id, workout_id, repeat, series) VALUES (2, 2, 7, 2);
-INSERT INTO saw_userExercise (exercise_id, workout_id, repeat, series) VALUES (1, 2, 12, 4);
-INSERT INTO saw_userweight (user_id_userid, date, weight_kg) VALUES (1,'2016-10-11', 64);
-INSERT INTO saw_userinfo (userid, weight_id) VALUES (1, 1);
-INSERT INTO saw_usernotes (userid, date, note)VALUES (1, '2016-10-23', 'notatka pierwsza jakas tam blalblablalbablabl');
-INSERT INTO saw_usernotes (userid, date, note)VALUES (1, '2016-09-26', 'njklsadhaslkdjlaksdjlaskdjlaksjdlakdjlskajd');
+INSERT INTO workout(user_details_id, date) VALUES (1, '2015-02-02');
+INSERT INTO workout(user_details_id, date) VALUES (1, '2015-03-03');
+INSERT INTO exercise (name, exercise_type) VALUES ('Przysiady', 'LEGS');
+INSERT INTO exercise (name, exercise_type) VALUES ('Przysiady przednie', 'LEGS');
+INSERT INTO exercise (name, exercise_type) VALUES ('Wykroki', 'LEGS');
+INSERT INTO user_exercise (exercise_id, workout_id, repeat, series) VALUES (1, 1, 10, 3);
+INSERT INTO user_exercise (exercise_id, workout_id, repeat, series) VALUES (2, 1, 10, 3);
+INSERT INTO user_exercise (exercise_id, workout_id, repeat, series) VALUES (3, 1, 10, 3);
+INSERT INTO user_exercise (exercise_id, workout_id, repeat, series) VALUES (2, 2, 7, 2);
+INSERT INTO user_exercise (exercise_id, workout_id, repeat, series) VALUES (1, 2, 12, 4);
+INSERT INTO user_weight (user_details_id, date, weight_kg) VALUES (1,'2016-10-11', 64);
+INSERT INTO user_info (user_details_id, weight_id) VALUES (1, 1);
+INSERT INTO user_notes (user_details_id, date, note)VALUES (1, '2016-10-23', 'notatka pierwsza jakas tam blalblablalbablabl');
+INSERT INTO user_notes (user_details_id, date, note)VALUES (1, '2016-09-26', 'njklsadhaslkdjlaksdjlaskdjlaksjdlakdjlskajd');
 
 
