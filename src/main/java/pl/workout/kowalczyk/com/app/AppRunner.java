2c45343eea166f526d6f954246e9eaf681a18ad6
Jakub Kowalczyk <beufort@o2.pl>
2017-01-22 16:10:26 +0100

    Optimized imports

--- src/main/java/pl/workout/kowalczyk/com/app/AppRunner.java
+++ src/main/java/pl/workout/kowalczyk/com/app/AppRunner.java
@@ -2,7 +2,6 @@ package pl.workout.kowalczyk.com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * Created by JK on 2017-01-02.
