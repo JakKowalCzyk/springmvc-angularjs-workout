cd6ce49aa5805ca5cc933ee4dc4a3a6cd489427b
Jakub Kowalczyk <beufort@o2.pl>
2017-01-24 17:38:09 +0100

    Deleted error-controller from swagger

--- src/main/java/pl/workout/kowalczyk/com/app/configuration/SwaggerConfiguration.java
+++ src/main/java/pl/workout/kowalczyk/com/app/configuration/SwaggerConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -12,6 +11,9 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
+
+import static com.google.common.base.Predicates.not;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * Created by JK on 2017-01-22.
@@ -25,7 +27,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(not(regex("/error")))
                 .build()
                 .pathMapping("/").groupName("Api")
                 .directModelSubstitute(LocalDate.class, String.class)
