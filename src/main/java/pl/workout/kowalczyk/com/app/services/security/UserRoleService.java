82d5e52cea3e57799f3a2dd7f6e4e18b95cba1ee
Jakub Kowalczyk <beufort@o2.pl>
2017-04-04 21:00:20 +0200

    created UserRoleService

---
+++ src/main/java/pl/workout/kowalczyk/com/app/services/security/UserRoleService.java
@@ -0,0 +1,10 @@
+package pl.workout.kowalczyk.com.app.services.security;
+
+import pl.workout.kowalczyk.com.app.model.BO.security.UserRole;
+import pl.workout.kowalczyk.com.app.services.ModelService;
+
+/**
+ * Created by JK on 2017-04-04.
+ */
+public interface UserRoleService extends ModelService<UserRole> {
+}
---
+++ src/main/java/pl/workout/kowalczyk/com/app/services/security/impl/UserRoleServiceImpl.java
@@ -0,0 +1,18 @@
+package pl.workout.kowalczyk.com.app.services.security.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.workout.kowalczyk.com.app.dao.security.UserRoleDao;
+import pl.workout.kowalczyk.com.app.model.BO.security.UserRole;
+import pl.workout.kowalczyk.com.app.services.impl.ModelServiceImpl;
+import pl.workout.kowalczyk.com.app.services.security.UserRoleService;
+
+/**
+ * Created by JK on 2017-04-04.
+ */
+public class UserRoleServiceImpl extends ModelServiceImpl<UserRole> implements UserRoleService {
+
+    @Autowired
+    public UserRoleServiceImpl(UserRoleDao baseDao) {
+        super(baseDao);
+    }
+}
