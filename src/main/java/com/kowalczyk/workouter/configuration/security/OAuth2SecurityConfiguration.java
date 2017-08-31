61112d30b0ebdeaa96291bd3b2f276572ca2f95f
Kuba Kowalczyk <kubabrag12>
2017-08-31 13:11:25 +0200

    Adjusted tests to security

--- src/main/java/com/kowalczyk/workouter/configuration/security/OAuth2SecurityConfiguration.java
+++ src/main/java/com/kowalczyk/workouter/configuration/security/OAuth2SecurityConfiguration.java
@@ -6,6 +6,7 @@ import com.kowalczyk.workouter.services.user.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -33,6 +34,7 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Profile("!test")
 public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private ClientDetailsService clientDetailsService;
