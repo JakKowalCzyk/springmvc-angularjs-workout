8a1ad4eeadcced978970e4ba12ca13015c625bff
Kuba Kowalczyk <kubabrag12>
2017-08-31 12:09:21 +0200

    Token validity time can be provided as env variable

--- src/main/java/com/kowalczyk/workouter/configuration/security/AuthorizationServerConfiguration.java
+++ src/main/java/com/kowalczyk/workouter/configuration/security/AuthorizationServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.kowalczyk.workouter.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -18,15 +19,20 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter{
     private static final String RESOURCE_ID = "workout_api";
-    private static String REALM = "WORKOUT_REALM";
+    private static final String CLIENT_SECRET = "secret";
+    private static final String REALM = "WORKOUT_REALM";
+    private static final String WORKOUT_CLIENT = "workout_client";
     @Autowired
     private TokenStore tokenStore;
-
     @Autowired
     private UserApprovalHandler userApprovalHandler;
-
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Value("#{environment.ACCESS_TOKEN}")
+    private String accessTokenValidity = "600";
+    @Value("#{environment.REFRESH_TOKEN}")
+    private String refreshTokenValidity = "900";
+
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -36,14 +42,14 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("workout_client")
+                .withClient(WORKOUT_CLIENT)
                 .resourceIds(RESOURCE_ID)
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
                 .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
                 .scopes("read", "write", "trust")
-                .secret("secret")
-                .accessTokenValiditySeconds(120)
-                .refreshTokenValiditySeconds(600);
+                .secret(CLIENT_SECRET)
+                .accessTokenValiditySeconds(Integer.valueOf(accessTokenValidity))
+                .refreshTokenValiditySeconds(Integer.valueOf(refreshTokenValidity));
     }
 
     @Override
