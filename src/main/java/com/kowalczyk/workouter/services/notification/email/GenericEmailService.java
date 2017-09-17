d8dd135ff03fb8ef58e5c5c7322f39e22e6baf59
Jakub Kowalczyk <beufort@o2.pl>
2017-09-17 16:14:22 +0200

    ConfirmationEmailService finished

--- src/main/java/com/kowalczyk/workouter/services/notification/email/GenericEmailService.java
+++ src/main/java/com/kowalczyk/workouter/services/notification/email/GenericEmailService.java
@@ -1,7 +1,22 @@
 package com.kowalczyk.workouter.services.notification.email;
 
+import com.kowalczyk.workouter.model.BO.user.User;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 /**
  * Created by JK on 2017-09-16.
  */
 public interface GenericEmailService {
+
+    MimeMessage prepareMessage(String email, String body, String subject) throws MessagingException;
+
+    String getEmailTo(User user);
+
+    String getSubject();
+
+    String getBody(String textToInclude);
+
+    void sendMessage(MimeMessage mimeMessage);
 }
